e5b2d96d1560cd9420466fcfd16ab4ee1611eb86
S. Schraml <ssc>
2024-12-12 09:09:32 +0100

    added training project of 24/12/10

--- project/app.ts
+++ project/app.ts
@@ -1,4 +1,15 @@
 import { Queue } from "./queue";
+import express from "express";
 
 const stringQueue = new Queue<string>();
 const boolQueue = new Queue<boolean>();
+
+const app = express();
+
+app.get('/greeting', (request, response) => {
+   response.send('Hallo Client');
+ })
+
+
+const port = 3000;
+app.listen(port, () => console.log(`Started server at port ${port}`));
