b93a86303e23e5d9578250783b548d94a7bb4487
Markus Haslinger <markus@haslinger.it>
2024-12-16 15:26:23 +0100

    started map

--- project/app.ts
+++ project/app.ts
@@ -1,8 +1,30 @@
 import { Queue } from "./queue";
 import express from "express";
+import {readFile} from "fs/promises";
+
+type QuestionOption = {
+    text: string,
+    isCorrect: boolean
+};
+type SeriesQuestion = {
+    series: string,
+    questions: QuestionOption[]
+}
 
 const stringQueue = new Queue<string>();
 const boolQueue = new Queue<boolean>();
+
+const seriesRawData = await readFile('series.json', 'utf-8');
+const series = JSON.parse(seriesRawData);
+const questions: SeriesQuestion[] = series.questions;
+
+const map: Map<string, SeriesQuestion[]> = new Map<string, SeriesQuestion[]>();
+for (const q of questions){
+    if (map.has(q.series)){
+        for (const o of q)
+        map.get(q.series)?.push(q.)
+    }
+}
 
 const app = express();
 
