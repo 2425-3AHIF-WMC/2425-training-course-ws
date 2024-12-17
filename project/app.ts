29c70f04390ee7279d12d6115bba2eb4d863b998
Markus Haslinger <markus@haslinger.it>
2024-12-17 09:31:31 +0100

    implement a sample map usage

--- project/app.ts
+++ project/app.ts
@@ -1,37 +1,51 @@
 import { Queue } from "./queue";
 import express from "express";
-import {readFile} from "fs/promises";
+import { readFile } from "fs/promises";
 
-type QuestionOption = {
+type Option = {
     text: string,
     isCorrect: boolean
 };
-type SeriesQuestion = {
+type SeriesQuestions = {
     series: string,
-    questions: QuestionOption[]
+    question: string,
+    options: Option[]
 }
+type SeriesQuestion = {
+    question: string,
+    options: Option[]
+};
 
 const stringQueue = new Queue<string>();
 const boolQueue = new Queue<boolean>();
 
-const seriesRawData = await readFile('series.json', 'utf-8');
+const seriesRawData = await readFile("series.json", "utf-8");
 const series = JSON.parse(seriesRawData);
-const questions: SeriesQuestion[] = series.questions;
+const questions: SeriesQuestions[] = series.questions;
 
 const map: Map<string, SeriesQuestion[]> = new Map<string, SeriesQuestion[]>();
-for (const q of questions){
-    if (map.has(q.series)){
-        for (const o of q)
-        map.get(q.series)?.push(q.)
+for (const q of questions) {
+    if (map.has(q.series)) {
+        const existingQuestions = map.get(q.series)!;
+        existingQuestions.push({
+            question: q.question,
+            options: q.options
+        });
+    } else {
+        map.set(q.series, [
+            {
+                question: q.question,
+                options: q.options
+            }
+        ]);
     }
 }
 
 const app = express();
 
-app.get('/greeting', (request, response) => {
-   response.send('Hallo Client');
- })
-
+app.get("/greeting", (request, response) => {
+    response.send("Hallo Client");
+});
 
 const port = 3000;
 app.listen(port, () => console.log(`Started server at port ${port}`));
